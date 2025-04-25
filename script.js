0c19e8b615164dca3c46909289e9a5ffd55d5d09
pHong1807 <phongtan1098@gmail.com>
2025-04-25 15:59:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function increase() {
+    let qty = document.getElementById("qty");
+    if (parseInt(qty.value) < 100) qty.value = parseInt(qty.value) + 1;
+  }
+  
+  function decrease() {
+    let qty = document.getElementById("qty");
+    if (parseInt(qty.value) > 1) qty.value = parseInt(qty.value) - 1;
+  }
+  
